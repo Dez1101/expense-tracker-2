83986c84c9fc1e09eff01ebbc4e830ce74cf742b
Desire Kabwanga <dkay1101@gmail.com>
2023-05-16 14:40:30 +0200

    2nd push

---
+++ src/Components/Expenses/ExpenseItem.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title)
+   const buttonClick =() =>{
+    setTitle('New Item')
+  }
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+      <button onClick={buttonClick}>Change Title</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
